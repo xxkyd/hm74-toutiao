e07ddb0d5f7d31ef4dcf9705e99a779b0d8a720b
kuyedie <1055188318@qq.com>
2019-07-09 17:52:02 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 安装element-ui
+import ELEMENT from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ELEMENT)
+
 Vue.config.productionTip = false
+
+// 职责1：导入项目需要依赖的资源（包css...）
+// 职责2：初始化根实例 new Vue() 选项 render 作用是把某一个组件渲染在你指定的容器内
 
 new Vue({
   render: h => h(App)
