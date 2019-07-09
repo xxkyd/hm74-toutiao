3fb55615786de10b4536b08146318b5c2abf2c32
kuyedie <1055188318@qq.com>
2019-07-09 18:20:47 +0800

    配置vue-router路由

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 // 安装element-ui
 import ELEMENT from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+// 默认索引：文件夹下存在index.js index.vue index.json默认加载这些文件
+// 如果同时存在三种（顺序index.js index.vue index.json）
+// 在使用vue-cli的时候 @符号在路径中作为前缀使用的时候，代表src目录
+import router from './router'
 Vue.use(ELEMENT)
 
 Vue.config.productionTip = false
@@ -12,5 +17,6 @@ Vue.config.productionTip = false
 // 职责2：初始化根实例 new Vue() 选项 render 作用是把某一个组件渲染在你指定的容器内
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
