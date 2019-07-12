e3f792adcbe6285070ad0d7a0c56bd43df006a19
kuyedie <1055188318@qq.com>
2019-07-12 15:46:27 +0800

    404和欢迎页面,左侧路由

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,6 +3,7 @@
     <el-aside class="my-aside"  :width="isCollapse?'64px':'200px'">
       <div class="logo" :class="{close:isCollapse}"></div>
         <el-menu
+        router
          style="border-right:none"
         default-active="1"
         background-color="#002033"
@@ -11,31 +12,31 @@
         :collapse="isCollapse"
         :collapse-transition="false"
         >
-          <el-menu-item index="1">
+          <el-menu-item index="/">
             <i class="el-icon-s-home"></i>
             <span slot="title">首页</span>
           </el-menu-item>
-          <el-menu-item index="2">
+          <el-menu-item index="/article">
             <i class="el-icon-document"></i>
             <span slot="title">内容管理</span>
           </el-menu-item>
-          <el-menu-item index="3">
+          <el-menu-item index="/image">
             <i class="el-icon-picture"></i>
             <span slot="title">素材管理</span>
           </el-menu-item>
-          <el-menu-item index="4">
+          <el-menu-item index="/publish">
             <i class="el-icon-s-promotion"></i>
             <span slot="title">发布文章</span>
           </el-menu-item>
-          <el-menu-item index="5">
+          <el-menu-item index="/comment">
             <i class="el-icon-chat-dot-round"></i>
             <span slot="title">评论管理</span>
           </el-menu-item>
-          <el-menu-item index="6">
+          <el-menu-item index="/fans">
             <i class="el-icon-present"></i>
             <span slot="title">粉丝管理</span>
           </el-menu-item>
-          <el-menu-item index="7">
+          <el-menu-item index="/setting">
             <i class="el-icon-setting"></i>
             <span slot="title">个人设置</span>
           </el-menu-item>
@@ -63,7 +64,9 @@
           </el-dropdown-menu>
         </el-dropdown>
       </el-header>
-      <el-main>Main</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -104,7 +107,7 @@ export default {
     }
   }
   .my-header {
-    border-bottom: 1px solid ddd;
+    border-bottom: 1px solid #ddd;
     line-height: 60px;
     .el-icon-s-fold {
       vertical-align: middle;
---
+++ src/views/welcome/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="welcome-container">
+        <img src="../../assets/images/welcome.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.welcome-container{
+    text-align: center;
+}
+</style>
