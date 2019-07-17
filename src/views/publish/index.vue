98d534e927b26f53c38d6b711dc349ffaffc934a
kuyedie <1055188318@qq.com>
2019-07-17 19:28:30 +0800

    发布文章页面

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,13 +1,72 @@
 <template>
-    <div>public</div>
+  <div class="container">
+    <el-card>
+      <div slot="header">
+        <my-bread>发布文章</my-bread>
+      </div>
+      <el-form :model="articleForm">
+        <el-form-item label="标题：">
+          <el-input v-model="articleForm.title" style="width:400px;"></el-input>
+        </el-form-item>
+        <el-form-item label="内容：">
+            <quill-editor v-model="articleForm.content"></quill-editor>
+        </el-form-item>
+        <el-form-item label="封面：">
+          <el-radio-group v-model="articleForm.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+          <div class="img-btn">
+            <img src="../../assets/images/default.png" alt />
+          </div>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <my-channel v-model="articleForm.channer_id"></my-channel>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">发表</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
 export default {
-
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      // 提交给后台的数据
+      articleForm: {
+        title: '',
+        cover: {
+          type: 1
+        }
+      },
+      channer_id: null
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang='less'>
+.img-btn {
+  width: 150px;
+  height: 150px;
+  border: 1px dashed #ddd;
+  img {
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+}
 </style>
